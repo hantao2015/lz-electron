a5f82ef0155f4eeaab940a0209b8be6d28f56b8a
hantao2015 <hantao@realsun.biz>
2019-08-28 21:06:51 +0800

    添加了hrapp的协议支持

--- app/main.dev.js
+++ app/main.dev.js
@@ -25,14 +25,14 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-let HelloElectron = edge.func({
-  assemblyFile: `./resources/Electron.dll`,
-  typeName: 'Electron.Electron',
-  methodName: 'HelloElectron'
-})
+// let HelloElectron = edge.func({
+//   assemblyFile: `./resources/Electron.dll`,
+//   typeName: 'Electron.Electron',
+//   methodName: 'HelloElectron'
+// })
 
 let mainWindow = null;
-
+let deeplinkingUrl="";
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
@@ -45,17 +45,18 @@ if (
   require('electron-debug')();
 }
 
-const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
 
-  return Promise.all(
-    extensions.map(name => installer.default(installer[name], forceDownload))
-  ).catch(console.log);
-};
+// const installExtensions = async () => {
+//   const installer = require('electron-devtools-installer');
+//   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+//   const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
 
+//   return Promise.all(
+//     extensions.map(name => installer.default(installer[name], forceDownload))
+//   ).catch(console.log);
+// };
 
+app.setAsDefaultProtocolClient('hrapp')
 /**
  * Add event listeners...
  */
@@ -73,7 +74,7 @@ app.on('ready', async () => {
     process.env.NODE_ENV === 'development' ||
     process.env.DEBUG_PROD === 'true'
   ) {
-    await installExtensions();
+    //await installExtensions();
   }
   
   mainWindow = new BrowserWindow({
@@ -88,18 +89,39 @@ app.on('ready', async () => {
     minimizable:true
 
   });
+  function logEverywhere(s) {
+    console.log(s)
+    if (mainWindow && mainWindow.webContents) {
+        mainWindow.webContents.executeJavaScript(`console.log("${s}")`)
+    }
+}
+  // Protocol handler for win32
+  if (process.platform == 'win32') {
+    // Keep only command line / deep linked arguments
+    deeplinkingUrl = process.argv.slice(1).toString();
+  }
   
- // mainWindow.webContents.openDevTools();
-  console.log(app.getVersion());
-  HelloElectron(3, function (error, result) {
+  //mainWindow.webContents.openDevTools();
+  
+  //console.log(app.getVersion());
+  // HelloElectron(3, function (error, result) {
 
-    if (error) throw error
-    //mainWindow.loadURL(result);
-    console.log(result)
-  })
- 
-  mainWindow.loadURL(`http://kingofdinner.realsun.me:1203`);
-  
+  //   if (error) throw error
+  //   //mainWindow.loadURL(result);
+  //   console.log(result)
+  // })
+  //deeplinkingUrl="hrapp://openop/http://ddss";
+
+  if (deeplinkingUrl && deeplinkingUrl.indexOf(":")>0)
+  {
+    deeplinkingUrl=deeplinkingUrl.split(":").slice(1);
+  }
+  let baseUrl="http://kingofdinner.realsun.me:1203/"
+  baseUrl="http://wux-hr03.china.ads.finisar.com:1002/"
+  let url=baseUrl+deeplinkingUrl
+  //mainWindow.loadURL(`http://wux-hr03.china.ads.finisar.com:1002`);
+  mainWindow.loadURL(url);
+  //logEverywhere(url)
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
   mainWindow.webContents.on('did-finish-load', () => {
@@ -121,12 +143,8 @@ app.on('ready', async () => {
     mainWindow = null;
   });
 
-  //const menuBuilder = new MenuBuilder(mainWindow);
- // menuBuilder.buildMenu();
 
-  // Remove this if your app does not use auto updates
-  // eslint-disable-next-line
- // new AppUpdater();
+  
  log.transports.file.level = 'info';
  autoUpdater.logger = log;
  autoUpdater.checkForUpdatesAndNotify();
